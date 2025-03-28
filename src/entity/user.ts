c25d2912c3dadcd4fdfaf728e416657650979db3
unknown <YY417@ccs.local>
2025-03-28 19:21:06 +0800

    Installed typeorm and added user.ts configuration

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -2,5 +2,24 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
 
+    @Column()
+    email!: string;
+
+    @Column()
+    title!: string;
+
+    @Column()
+    firstName!: string;
+
+    @Column()
+    lastName!: string;
+
+    @Column()
+    passwordHash!: string;
+
+    @Column()
+    role!: string;
 }
